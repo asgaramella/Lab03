cb2bb4bbc0329ad31940724f998ae84776af8f7b
samp <samp@lepcopi6.leplab.polito.it>
2017-03-22 14:44:31 +0100

    seconda versione

--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -4,13 +4,13 @@ import java.io.*;
 import java.util.*;
 
 public class Dictionary {
-	private List<RichWord> dizionario;
+	private List<String> dizionario;
 	
 	
 
 	public Dictionary() {
 		super();
-		dizionario=new ArrayList<RichWord>();
+		dizionario=new ArrayList<String>();
 	}
 	
 	
@@ -21,7 +21,7 @@ public class Dictionary {
 	BufferedReader br = new BufferedReader(fr);
 	String word;
 	while ((word = br.readLine()) != null) {
-	 dizionario.add(new RichWord(word));
+	 dizionario.add(word);
 	}
 	br.close();
 	} catch (IOException e){
@@ -38,18 +38,48 @@ public class Dictionary {
 	 */
 	public List<RichWord> spellCheckText(List<String> inputTextList){
 		List<RichWord> ltemp=new ArrayList<RichWord>();
-		for(String s: inputTextList){
+		Collections.sort(dizionario);
+	
+		for(String s:inputTextList){
+			boolean trovata=false;
 			RichWord rtemp=new RichWord(s);
 			ltemp.add(rtemp);
-		
-			if (dizionario.contains(rtemp))
-				rtemp.setCorretta(true);
-
+			int i=0;
+			int f=dizionario.size();
+			int m=dizionario.size()/2;
+			while(i<f && trovata==false){
+				String stemp=dizionario.get(m);
+				System.out.println(stemp);
+				System.out.println(i);
+				System.out.println(f);
+				System.out.println();
 				
+				if(s.compareTo(stemp)>0)
+					i= m +1;
+				else{
+					if(s.compareTo(stemp)<0)
+						f=m -1;
+					else
+						trovata=true;
+				}
+			
+				m=(i+f)/2;
+				
+				if (i+1==f){
+					trovata=false;
+				}
+				
+			}
+		
+			
+			
+			if (trovata==true)
+				rtemp.setCorretta(true);
 			else
 				rtemp.setCorretta(false);
+			}
 				
-		}
+		
 		return ltemp;
 	}
 	
